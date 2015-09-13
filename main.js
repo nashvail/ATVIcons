5fc34ea348f596edc789de18a5e0cf812f9981d0
nashmaniac <nishantve1@gmail.com>
2015-09-13 14:00:17 +0530

    got the perspective

--- main.js
+++ main.js
@@ -1,38 +1,93 @@
 (function() {
 	'use strict';
 
-	var $box = $('.box'),
+	var $box = $('.icon'),
 			offset = $box.offset(),
 			width = $box.width(),
 			height = $box.height(),
 			mouseOffsetInside = {},
-			perspectiveAmount = 600;
+			perspectiveAmount = 800;
 
-	var maxRotation = 8;
-	var maxTranslation = 5;
+	var maxRotation = 4;
+	var maxTranslation = 4;
 
 
 	$box.on('mousemove', function(e) {
-		// Calculate the mouse offset inside this thing 
-		// animate the main then its children like that 
 		mouseOffsetInside = {
 			x : e.pageX - offset.left,
 			y : e.pageY - offset.top
 		};
 
-		var xRotationPercentage = calculateRotationAmount(mouseOffsetInside.y, height);
-		var yRotationPercentage = calculateRotationAmount(mouseOffsetInside.x, width) * -1;
+		var xRotationPercentage = calculateRotationPercentage(mouseOffsetInside.y, height);
+		var yRotationPercentage = calculateRotationPercentage(mouseOffsetInside.x, width) * -1;
 
-		var rotate = "perspective(" + perspectiveAmount +"px) rotateX(" + xRotationPercentage * maxRotation + "deg)" +  " rotateY(" + yRotationPercentage * maxRotation+  "deg)";
-		var translate = " translate3d(" + xRotationPercentage * maxTranslation + "px," + yRotationPercentage * maxTranslation + "px, 0px)";
-		$(this).css({
-			'transform' : rotate + translate
-		})
+		var xTranslationPercentage = calculateTranslationPercentage(mouseOffsetInside.x, width);
+		var yTranslationPercentage = calculateTranslationPercentage(mouseOffsetInside.y, height);
+
+		appleTvAnimate($(this).children('.box'), perspectiveAmount, {
+			maxTranslationX : maxTranslation,
+			maxTranslationY : maxTranslation,
+			maxRotationX : maxRotation,
+			maxRotationY : maxRotation,
+			xRotationPercentage : xRotationPercentage,
+			yRotationPercentage : yRotationPercentage,
+			xTranslationPercentage : xTranslationPercentage,
+			yTranslationPercentage : yTranslationPercentage
+		});
+
+		appleTvAnimate($(this).children('.shadow'), 0, {
+			maxTranslationX : maxTranslation * 2,
+			maxTranslationY : maxTranslation * 2,
+			maxRotationX : maxRotation,
+			maxRotationY : maxRotation,
+			xRotationPercentage : xRotationPercentage,
+			yRotationPercentage : yRotationPercentage,
+			xTranslationPercentage : xTranslationPercentage,
+			yTranslationPercentage : yTranslationPercentage
+		});
+
+		appleTvAnimate($(this).children('.title'), 600, {
+			maxTranslationX : maxTranslation * 0.65,
+			maxTranslationY : maxTranslation * 0.65,
+			maxRotationX : maxRotation,
+			maxRotationY : maxRotation,
+			xRotationPercentage : xRotationPercentage,
+			yRotationPercentage : yRotationPercentage,
+			xTranslationPercentage : xTranslationPercentage,
+			yTranslationPercentage : yTranslationPercentage 
+		});
+
+		appleTvAnimate($(this).children('.subtitle'), 600, {
+			maxTranslationX : maxTranslation * 0.8,
+			maxTranslationY : maxTranslation * 0.8 ,
+			maxRotationX : maxRotation,
+			maxRotationY : maxRotation,
+			xRotationPercentage : xRotationPercentage,
+			yRotationPercentage : yRotationPercentage,
+			xTranslationPercentage : xTranslationPercentage,
+			yTranslationPercentage : yTranslationPercentage 
+		});
+
+
 
 	});
 
-	function calculateRotationAmount(offset, dimension) {
+	function appleTvAnimate(element, perspectiveAmount, config) {
+		
+		var rotate = "perspective(" + perspectiveAmount +"px) rotateX(" + config.xRotationPercentage * config.maxRotationX + "deg)" +  " rotateY(" + config.yRotationPercentage * config.maxRotationY+  "deg)";
+		var translate = " translate3d(" + config.xTranslationPercentage * config.maxTranslationX + "px," + config.yTranslationPercentage * config.maxTranslationY + "px, 0px)";
+		element.css({
+			'transform' : rotate + translate
+		})
+	}
+
+	function calculateRotationPercentage(offset, dimension) {
 		return ((-2 / dimension) * offset) + 1;
 	}
+
+	function calculateTranslationPercentage(offset, dimension) {
+		return ((-2 / dimension) * offset) + 1;
+	}
+
 
 })();
